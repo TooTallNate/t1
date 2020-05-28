cd521d17199d9ec9146eb2487446eff688ac02c1
Nathan Rajlich <nathan@tootallnate.net>
2020-05-27 20:52:17 -0700

    Progress point

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import {
 	LineChart,
 	CartesianGrid,
 	ResponsiveContainer,
+	TooltipFormatter,
+	TickFormatterFunction,
 	XAxis,
 	YAxis,
 	Tooltip,
@@ -10,37 +12,74 @@ import {
 } from 'recharts';
 import useSWR from 'swr';
 import fetch from 'isomorphic-fetch';
+import { timeFormat } from 'd3-time-format';
+
+const f = timeFormat('%H:%M:%S');
 
 export default function Index() {
-	const maxCount = 100;
+	//const maxCount = 100;
+	const maxCount = 36;
 	const { data, error } = useSWR(`/api/readings?maxCount=${maxCount}`, async (endpoint: string) => {
 		const res = await fetch(endpoint);
 		const body = await res.json();
 		return body;
 	});
 
-	if (!data) {
-		return null;
-	}
+	let mainChart = null;
+	if (data) {
+		for (const r of data.readings) {
+			r.date = new Date(r.date).getTime();
+		}
+		console.log(data.readings);
 
-	for (const r of data.readings) {
-		r.date = new Date(r.date);
+		const tooltipFormatter: TooltipFormatter = (value, name, entry, index) => {
+			return [ value, data.units ];
+		};
+
+		const tickFormatter: TickFormatterFunction = (value) => {
+			return f(value);
+		};
+
+		const CustomTooltip = ({ active, payload, label }) => {
+			if (active) {
+				return (
+					<div className="custom-tooltip">
+						<p className="time">Time: {f(label)}</p>
+						<p className="value">{data.units}: <span className="value">{payload[0].value}</span></p>
+					</div>
+				);
+			}
+
+			return null;
+		};
+
+		mainChart=
+			<ResponsiveContainer height="50%" width="100%">
+				<LineChart
+					data={data.readings}
+					margin={{ top: 5, right: 0, left: 30, bottom: 5 }}
+				>
+					<CartesianGrid strokeDasharray="3 9" />
+					<XAxis dataKey="date" tickFormatter={tickFormatter} />
+					<YAxis orientation="right" />
+					<Tooltip content={<CustomTooltip />}/>
+					<Line type="monotone" dataKey="value" stroke="#8884d8" />
+				</LineChart>
+			</ResponsiveContainer>
 	}
-	console.log(data.readings);
 
 	return (
-		<ResponsiveContainer aspect={4.0/3.0} width="100%">
-			<LineChart
-				data={data.readings}
-				margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-			>
-				<CartesianGrid strokeDasharray="3 3" />
-				<XAxis dataKey="date" />
-				<YAxis />
-				<Tooltip />
-				<Legend />
-				<Line type="monotone" dataKey="value" stroke="#8884d8" />
-			</LineChart>
-		</ResponsiveContainer>
+		<>
+			{mainChart}
+			<style jsx global>{`
+			  html, body, #__next {
+				margin: 0px;
+				padding: 0px;
+				width: 100%;
+				height: 100%;
+				font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+			  }
+			`}</style>
+		</>
 	);
 }
