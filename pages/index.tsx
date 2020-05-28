2c0bcf34bd69c9c31bfe7f2e2974a40d910352a1
Nathan Rajlich <nathan@tootallnate.net>
2020-05-27 18:22:19 -0700

    Progress point

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import {
 	LineChart,
 	CartesianGrid,
+	ResponsiveContainer,
 	XAxis,
 	YAxis,
 	Tooltip,
@@ -8,28 +9,38 @@ import {
 	Line
 } from 'recharts';
 import useSWR from 'swr';
+import fetch from 'isomorphic-fetch';
 
 export default function Index() {
-	const { data, error } = useSWR('/api/latest', window.fetch)
-	console.log({ data, error });
+	const maxCount = 100;
+	const { data, error } = useSWR(`/api/readings?maxCount=${maxCount}`, async (endpoint: string) => {
+		const res = await fetch(endpoint);
+		const body = await res.json();
+		return body;
+	});
 
-	return <div>{JSON.stringify(data)}</div>;
-	/*
+	if (!data) {
+		return null;
+	}
+
+	for (const r of data.readings) {
+		r.date = new Date(r.date);
+	}
+	console.log(data.readings);
+
 	return (
-		<LineChart
-			width={730}
-			height={250}
-			data={data}
-			margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-		>
-			<CartesianGrid strokeDasharray="3 3" />
-			<XAxis dataKey="name" />
-			<YAxis />
-			<Tooltip />
-			<Legend />
-			<Line type="monotone" dataKey="pv" stroke="#8884d8" />
-			<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-		</LineChart>
+		<ResponsiveContainer aspect={4.0/3.0} width="100%">
+			<LineChart
+				data={data.readings}
+				margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+			>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis dataKey="date" />
+				<YAxis />
+				<Tooltip />
+				<Legend />
+				<Line type="monotone" dataKey="value" stroke="#8884d8" />
+			</LineChart>
+		</ResponsiveContainer>
 	);
-	*/
 }
