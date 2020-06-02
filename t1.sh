8bb71476b79fa54d8f71bddc0fc9d40a914ec570
Nathan Rajlich <nathan@tootallnate.net>
2020-06-01 20:33:41 -0700

    Finish porting `t1_ps1`

--- t1.sh
+++ t1.sh
@@ -32,6 +32,9 @@ t1_daemon() {
 
 t1_ps1() {
 	local trend=
+	local seconds_ago=
+	local timeago_warn_seconds=
+	local timeago_alarm_seconds=
 	local color="$GREEN"
 	local cache_dir="$HOME/Library/Caches/io.n8.t1"
 	local env_file="$cache_dir/latest.env"
@@ -39,6 +42,18 @@ t1_ps1() {
 	eval "$(sed 's/^/local /' < "$env_file")"
 	local bgl="$t1_latest_reading_value"
 	local delta="$t1_latest_reading_delta"
+	seconds_ago="$(expr "$(date_now)" - "$t1_latest_reading_date")"
+
+	# TODO: make configurable
+	local timeago_warn_mins="15" # 15 minutes
+	local timeago_alarm_mins="30" # 30 minutes
+	local value_high_warn="180"
+	local value_high_alarm="230"
+	local value_low_warn="70"
+	local value_low_alarm="55"
+
+	timeago_warn_seconds="$(expr "$timeago_warn_mins" \* 60)"
+	timeago_alarm_seconds="$(expr "$timeago_alarm_mins" \* 60)"
 
 	if [ "$delta" -ge 0 ]; then
 		delta="+$delta"
@@ -51,40 +66,40 @@ t1_ps1() {
 		delta="$delta*"
 	fi
 
-	#if [ "${settings_alarm_timeago_urgent}" = "true" ] && [ "${mins_ago}" -ge "${settings_alarm_timeago_urgent_mins}" ]; then
-	#	trend='↛'
-	#	color="${INVERSE}${BOLD}${RED}"
-	#	bgl="$(echo "${bgl}" | string_strikethrough)"
-	#	delta="$(echo "${delta}" | string_strikethrough)"
-	#elif [ "${settings_alarm_timeago_warn}" = "true" ] && [ "${mins_ago}" -ge "${settings_alarm_timeago_warn_mins}" ]; then
-	#	trend='↛'
-	#	color="${INVERSE}${BOLD}${YELLOW}"
-	#	bgl="$(echo "${bgl}" | string_strikethrough)"
-	#	delta="$(echo "${delta}" | string_strikethrough)"
-	#else
-	case "$t1_latest_reading_trend" in
-		0) trend="⇼";; # None
-		1) trend="⇈";; # DoubleUp
-		2) trend="↑";; # SingleUp
-		3) trend="↗";; # FortyFiveUp
-		4) trend="→";; # Flat
-		5) trend="↘";; # FortyFiveDown
-		6) trend="↓";; # SingleDown
-		7) trend="⇊";; # DoubleDown
-		#8)            # NotComputable
-		#9)            # OutOfRange
-	esac
+	if [ "$seconds_ago" -ge "$timeago_alarm_seconds" ]; then
+		trend='↛'
+		color="${INVERSE}${BOLD}${RED}"
+		bgl="$(printf "%s" "${bgl}" | string_strikethrough)"
+		delta="$(printf "%s" "${delta}" | string_strikethrough)"
+	elif [ "$seconds_ago" -ge "$timeago_warn_seconds" ]; then
+		trend='↛'
+		color="${INVERSE}${BOLD}${YELLOW}"
+		bgl="$(echo "${bgl}" | string_strikethrough)"
+		delta="$(echo "${delta}" | string_strikethrough)"
+	else
+		case "$t1_latest_reading_trend" in
+			0) trend="⇼";; # None
+			1) trend="⇈";; # DoubleUp
+			2) trend="↑";; # SingleUp
+			3) trend="↗";; # FortyFiveUp
+			4) trend="→";; # Flat
+			5) trend="↘";; # FortyFiveDown
+			6) trend="↓";; # SingleDown
+			7) trend="⇊";; # DoubleDown
+			#8)            # NotComputable
+			#9)            # OutOfRange
+		esac
 
-	#	if [ "${t1_latest_reading_value}" -ge "${settings_thresholds_bg_high}" ]; then
-	#		color="${BOLD}${YELLOW}"
-	#	elif [ "${t1_latest_reading_value}" -ge "${settings_thresholds_bg_target_top}" ]; then
-	#		color="${YELLOW}"
-	#	elif [ "${t1_latest_reading_value}" -le "${settings_thresholds_bg_low}" ]; then
-	#		color="${BOLD}${RED}"
-	#	elif [ "${t1_latest_reading_value}" -le "${settings_thresholds_bg_target_bottom}" ]; then
-	#		color="${RED}"
-	#	fi
-	#fi
+		if [ "$t1_latest_reading_value" -ge "$value_high_alarm" ]; then
+			color="$BOLD$YELLOW"
+		elif [ "$t1_latest_reading_value" -ge "$value_high_warn" ]; then
+			color="$YELLOW"
+		elif [ "$t1_latest_reading_value" -le "$value_low_alarm" ]; then
+			color="$BOLD$RED"
+		elif [ "$t1_latest_reading_value" -le "$value_low_warn" ]; then
+			color="$RED"
+		fi
+	fi
 
 	if [ "$t1_latest_reading_trend" -eq 8 ]; then
 		color="$BOLD$RED"
