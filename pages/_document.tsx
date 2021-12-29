dc5e6153ca5fcad61bbb0eb37247bc22a7d42eb1
Nathan Rajlich <n@n8.io>
2021-12-29 20:04:53 -0300

    Use `next-pwa`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,3 @@
-// pages/_document.js
-
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import theme from '@components/theme';
@@ -8,7 +6,31 @@ export default class Document extends NextDocument {
 	render() {
 		return (
 			<Html lang="en">
-				<Head />
+				<Head>
+					<meta charSet="utf-8" />
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+					<meta
+						name="viewport"
+						content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+					/>
+					<meta name="description" content="Description" />
+					<meta name="keywords" content="Keywords" />
+					<link rel="manifest" href="/manifest.json" />
+					<link
+						href="/icons/favicon-16x16.png"
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+					/>
+					<link
+						href="/icons/favicon-32x32.png"
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+					/>
+					<link rel="apple-touch-icon" href="/apple-icon.png"></link>
+					<meta name="theme-color" content="#317EFB" />
+				</Head>
 				<body>
 					<ColorModeScript
 						initialColorMode={theme.config.initialColorMode}
