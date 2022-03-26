cd09f10a5ae4ee2bf73266089deceddd3503cf3b
Nathan Rajlich <n@n8.io>
2022-03-26 14:17:17 -0700

    Fix icon href

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,13 +17,13 @@ export default class Document extends NextDocument {
 					<meta name="keywords" content="Keywords" />
 					<link rel="manifest" href="/manifest.json" />
 					<link
-						href="/icons/favicon-16x16.png"
+						href="/icons/icon-16x16.png"
 						rel="icon"
 						type="image/png"
 						sizes="16x16"
 					/>
 					<link
-						href="/icons/favicon-32x32.png"
+						href="/icons/icon-32x32.png"
 						rel="icon"
 						type="image/png"
 						sizes="32x32"
